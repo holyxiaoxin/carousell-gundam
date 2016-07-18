921e878966f14f5a3cbd168b304072db3df65539
Jia Rong <jr.loves.maths@gmail.com>
2016-07-18 14:23:09 +0800

    Add localhost contraint

--- rails-app/config/routes.rb
+++ rails-app/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-  resources :watchlists
+  resources :watchlists, :constraints => {:ip => /127.0.0.1/}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
